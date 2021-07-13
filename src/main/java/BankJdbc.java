e35182033d4a73cecb44d0fffa854fb07f906906
pablojg9 <pablojuniorgn2@gmail.com>
2021-07-13 10:18:49 -0300

    usando posgresql jdbc - 13/07/2021

--- src/main/java/BankJdbc.java
+++ src/main/java/BankJdbc.java
@@ -1,11 +1,19 @@
-import connection.jdbc.SingleConnection;
+import Dao.UserDao;
+import model.UserModel;
 import org.junit.Test;
 
 public class BankJdbc {
 
     @Test
     public void initBank() {
-        SingleConnection.getConnection();
+        UserDao userDao = new UserDao();
+        UserModel userModel = new UserModel();
+
+        userModel.setId(5L);
+        userModel.setNome("keyla");
+        userModel.setEmail("keyla@gmail.com");
+
+        userDao.save(userModel);
     }
 
 }
--- src/main/java/Dao/UserDao.java
+++ src/main/java/Dao/UserDao.java
@@ -1,8 +1,11 @@
 package Dao;
 
 import connection.jdbc.SingleConnection;
+import model.UserModel;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class UserDao {
 
@@ -10,5 +13,31 @@ public class UserDao {
 
     public UserDao() {
         connection = SingleConnection.getConnection();
+    }
+
+    public void save(UserModel user) {
+        try {
+            String sql = "insert into useposjava (idtable, nome, email) values (?, ?, ?)";
+            PreparedStatement insert = connection.prepareStatement(sql);
+            insert.setLong(1, user.getId());
+            insert.setString(2, user.getNome());
+            insert.setString(3, user.getEmail());
+            insert.execute();
+            connection.commit();
+
+        } catch (Exception e) {
+            try {
+                connection.rollback();
+            }catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+
+
+            e.printStackTrace();
+        }
+
+
+
+
     }
 }
